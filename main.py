d4253815d4bd313fbabbc9eae1b2818cee044391
Florent Didascalou <florentd@uw.edu>
2023-02-14 00:03:32 +0100

    Many changes about plotting

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+
+"""
 import math
 import random
 import matplotlib.pyplot as plt
@@ -10,7 +13,7 @@ class Particle:
     Class for a particle
     """
 
-    def __init__(self, id, minimum, maximum):
+    def __init__(self, id, minimum, maximum, v_max):
         """
         Constructor for a particle
 
@@ -19,9 +22,9 @@ class Particle:
         """
         self.id = id
         self.pos = (random.uniform(minimum, maximum), random.uniform(minimum, maximum))
-        self.v = (random.uniform(minimum / 25, maximum / 25), random.uniform(minimum / 25, maximum / 25))
+        self.v = (random.uniform(minimum * v_max, maximum * v_max), random.uniform(minimum * v_max, maximum * v_max))
         self.f = 2 ** 32
-
+        self.v_max = v_max
         # Particle's best location
         self.sp_best = None
         # Global best location
@@ -38,7 +41,7 @@ def v_p(particle):
     :param particle: Particle object for which the new velocity needs to be calculated
     :return: Returns a new tuple with the particle's new velocity in the x and y direction
     """
-
+    v_max = particle.v_max
     R = random.uniform(0, 1)
 
     pos = np.array(particle.pos)
@@ -109,7 +112,7 @@ def cost_rastrigin(x, y, n=2):
     return (X**2 - 10 * np.cos(2 * np.pi * X)) + \
             (Y**2 - 10 * np.cos(2 * np.pi * Y)) + 20
 
-def update_gb(particle_list, cost_function, neighborhood = "global"):
+def update_gb(particle_list, cost_function, gb_best_global_cost, neighborhood = "global"):
     """
     Updates the global best position of all particles
 
@@ -126,9 +129,12 @@ def update_gb(particle_list, cost_function, neighborhood = "global"):
         index_lowest_cost = initial_costs.index(min(initial_costs))
         lowest_cost_particle = particle_list[index_lowest_cost]
 
+        if gb_best_global_cost > min(initial_costs):
+            gb_best_global_cost = min(initial_costs)
         # Initialise all global best positions
         for p in particle_list:
             p.gb_best = lowest_cost_particle.pos
+        return gb_best_global_cost
 
     elif neighborhood == "social":
 
@@ -192,6 +198,15 @@ def plot_graphs(cost_function):
 
     for n in range(tests - 1):
 
+        
+
+        # Build lists of x and y coordinates for each particle at a specific test run (denoted by n)
+        x = [particle_history[particle][n+1][0] for particle in particle_history]
+        y = [particle_history[particle][n+1][1] for particle in particle_history]
+        # reset the background back in the canvas state, screen unchanged
+        fig.canvas.restore_region(bg)
+
+        # set title and text
         ax.set_title(f"Particle convergence using PSO with {len(particle_history)} particles (iteration: {n+2})")
 
         text = f"a = {round(parameter_history[n+1][0], 2)}\n " \
@@ -199,12 +214,6 @@ def plot_graphs(cost_function):
                f"c = {round(parameter_history[n+1][2], 2)}\n"
 
         ax.text(3, 3, text, fontsize=10)
-
-        # Build lists of x and y coordinates for each particle at a specific test run (denoted by n)
-        x = [particle_history[particle][n+1][0] for particle in particle_history]
-        y = [particle_history[particle][n+1][1] for particle in particle_history]
-        # reset the background back in the canvas state, screen unchanged
-        fig.canvas.restore_region(bg)
         # update the artist, neither the canvas state nor the screen have changed
         ln.set_xdata(x)
         ln.set_ydata(y)
@@ -273,7 +282,8 @@ x_range = (-2, 2)
 y_range = (-1, 3)
 
 # Velocity cap
-v_max = 4 / 50
+#v_max = 0.07
+
 
 # Particle Swarm Optimisation (PSO) constants
 b_pso, c_pso = 2, 2
@@ -285,10 +295,12 @@ d = 0.5 / tests
 particle_history = {}
 parameter_history = {}
 
-particles = [Particle(x, -5, 5) for x in range(20)]
 
-if __name__ == '__main__':
-
+def runPSO(v_max):
+    particles = [Particle(x, -5, 5, v_max) for x in range(20)]
+    particle_history = {}
+    global a_pso
+    
     if cost_func == "rosenbrock":
         cost_function = cost_rosenbrock
     else:
@@ -303,9 +315,22 @@ if __name__ == '__main__':
         particle.sp_best = particle.pos
         particle_history[particle].append((particle.pos[0], particle.pos[1], particle.v[0], particle.v[1]))
 
+    gb_best_global_cost = 2**32
+    counter = 0
     for i in range(tests):
-        print("Iteration: " + str(i))
-        update_gb(particles, cost_function)
+        #print("Iteration: " + str(i))
+        update_gb(particles, cost_function, gb_best_global_cost)
+        
+        
+        cur_best = min([p.lowest_cost for p in particles])
+        if gb_best_global_cost > cur_best:
+            gb_best_global_cost = cur_best
+            counter = 0
+        elif counter < 50:
+            counter += 1
+        else:
+            print("Converged")
+            return gb_best_global_cost, i
 
         # Save history of parameters
         parameter_history[i] = (a_pso, b_pso, c_pso)
@@ -325,16 +350,117 @@ if __name__ == '__main__':
             particle.pos = (x_update, y_update)
             particle.v = (v_x_update, v_y_update)
             particle_history[particle].append((particle.pos[0], particle.pos[1], particle.v[0], particle.v[1]))
-
+        
         a_pso -= d
-
-    # Final positions and costs
+    print("run complete")
+    gb_best_final = 2**32
     for particle in particles:
-        print(particle.pos)
-        print(particle.f)
-        print()
+        score = particle.lowest_cost
+        if gb_best_final > score:
+            gb_best_final = score
+    return gb_best_final, 1000
 
-    plot_graphs(cost_function)
-    # print(particles[0].gb_best)
-    # plot_velocity()
-    # plot_positions()
+if __name__ == '__main__':
+
+    max_velocities = np.arange(0.01, 0.21, 0.01)
+    avg_gb_best = []
+    avg_itter_count = []
+    for vel in max_velocities:
+        print("velocity : " + str(vel))
+        hist_gb_best = []
+        itter_count = []
+        for i in range (30):
+            gb_run, itter = runPSO(vel)
+            hist_gb_best.append(gb_run)
+            itter_count.append(itter)
+        avg_gb_best.append(sum(hist_gb_best) / 30)
+        avg_itter_count.append(sum(itter_count) / 30)
+        
+    
+    #Store convergence data
+    f = open("velocity_convergence_data_Rosen_c10.txt", "w")
+    f.write("Velocities :\n" + str(list(max_velocities)) + "\nAvg_itter_count : " + str(avg_itter_count))
+
+    # store velocity data
+    f = open("velocity_experiment_data_Rosen_c10.txt", "w")
+    f.write("Velocities :\n" + str(list(max_velocities)) + "\nAvg_gb_best : " + str(avg_gb_best))
+    
+    # Parameter information
+    text = "a = 0.9, a = 0.4" + \
+            "\nb = 2" + \
+            "\nc = 10"
+    # Plot convergence data
+    fig2 = plt.figure()
+    plt.plot(max_velocities, avg_itter_count, "ob")
+    fig2.suptitle('Comparing performance of PSO on Rosenbrock with different Velocity Caps')
+    plt.xlabel('Max Velocity')
+    plt.ylabel('Average Itteration Count')
+    plt.text(0.025, 1040, text)
+    fig2.savefig('velVSitterRosen_c10.jpg')
+    plt.clf()
+    
+    # plot velocity data
+    fig1 = plt.figure()
+    plt.plot(max_velocities, avg_gb_best, "ob")
+    fig1.suptitle('Comparing performance of PSO on Rosenbrock with different Velocity Caps')
+    plt.xlabel('Max Velocity')
+    plt.ylabel('Average Final Cost')
+    plt.text(0.1, 3, text)
+    fig1.savefig('velVScostRosen_c10.jpg')
+
+    """
+    Comment out above and uncomment bellow to display the PSO animation
+    """
+    # v_max = 0.08
+    # particles = [Particle(x, -5, 5, v_max) for x in range(20)]
+    
+
+    # if cost_func == "rosenbrock":
+    #     cost_function = cost_rosenbrock
+    # else:
+    #     cost_function = cost_rastrigin
+
+    # # Initialise an empty list to contain each particle's history
+    # for particle in particles:
+    #     particle_history[particle] = []
+
+    # # Initialise all personal best positions to their initial position
+    # for particle in particles:
+    #     particle.sp_best = particle.pos
+    #     particle_history[particle].append((particle.pos[0], particle.pos[1], particle.v[0], particle.v[1]))
+
+    # for i in range(tests):
+    #     print("Iteration: " + str(i))
+    #     update_gb(particles, cost_function, 2**32)
+
+    #     # Save history of parameters
+    #     parameter_history[i] = (a_pso, b_pso, c_pso)
+
+    #     for particle in particles:
+
+    #         cost = cost_function(particle.pos[0], particle.pos[1])
+
+    #         if cost < particle.lowest_cost:
+    #             particle.lowest_cost = cost
+    #             particle.sp_best = particle.pos
+
+    #         x_update, y_update = s_p(particle)
+    #         v_x_update, v_y_update = v_p(particle)
+
+    #         particle.f = cost
+    #         particle.pos = (x_update, y_update)
+    #         particle.v = (v_x_update, v_y_update)
+    #         particle_history[particle].append((particle.pos[0], particle.pos[1], particle.v[0], particle.v[1]))
+
+    #     a_pso -= d
+
+    # # Final positions and costs
+    # for particle in particles:
+    #     print(particle.pos)
+    #     print(particle.f)
+    #     print()
+
+    # plot_graphs(cost_function)
+    # # print(particles[0].gb_best)
+    # # plot_velocity()
+    # # plot_positions()
